3210c0aea17dfaf4675bf9c59f86a5e1affe5c85
Young Jin Kim <kim687@usc.edu>
2019-03-18 21:47:02 -0700

    finished

---
+++ resources/views/document.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Document</title>
+    </head>
+    <body>
+        <div id = "text" contenteditable="true">
+            This text can be edited by the user.
+        </div>
+        <script src="{{ URL::to('js/main.js')}}"></script>
+    </body>
+</html>
