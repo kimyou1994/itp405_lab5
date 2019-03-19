794ec9e1cb73a40e1a51b27db4a97b5e47c34146
Young Jin Kim <kim687@usc.edu>
2019-03-18 22:57:45 -0700

    https

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://kimyou1994-itp405-lab5.herokuapp.com/docs'),
 
     'asset_url' => env('ASSET_URL', null),
 
